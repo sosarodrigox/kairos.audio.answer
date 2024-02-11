3cea5ef87064ebbeb8ed8e072b6d8fd1d51eac2b
sosarodrigox <sosarodrigox@gmail.com>
2024-02-11 17:23:12 -0300

    create player

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Player1 } from './components/player1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Kairos Audio Player</h1>
+        <Player1 />
       </header>
     </div>
   );
