6067335eaa55775f243c773fc8e7d63155a70357
sosarodrigox <sosarodrigox@gmail.com>
2024-02-18 10:41:22 -0300

    add gpt style chat

---
+++ server.js
@@ -0,0 +1,40 @@
+const PORT = process.env.PORT || 8000;
+const express = require('express');
+const cors = require('cors');
+const e = require('express');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = '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';
+
+app.listen(PORT, () => console.log(`Your server running on port ${PORT}`));
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: req.body.message
+                }
+            ],
+            max_tokens: 150,
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import ReactPlayerTest from './components/ReactPlayerTest';
 
+const App = () => {
 
+  const [message, setMessage] = useState(null)
+  const [value, setValue] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
 
+  const getMessages = async () => {
 
-function App() {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+
+    }
+
+    try {
+      const response = await fetch('http://localhost:8000/completions', options);
+      const data = await response.json();
+      // console.log(data);
+      setMessage(data.choices[0].message)
+    } catch (error) { console.error(error) };
+  }
+
+  console.log(message)
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <section className='side-bar'>
+        <button>+ New Chat</button>
+        <ul className='history'>
+          <li>Chat 1</li>
+          <li>Chat 2</li>
+          <li>Chat 3</li>
+        </ul>
+        <nav>
+          Contact Me
+        </nav>
+      </section>
+      <section className='main'>
+        <h1>PodChat V1.0</h1>
         <ReactPlayerTest />
-      </header>
+        <ul className='feed'>
+
+        </ul>
+        <div className='bottom-section'>
+          <div className='input-container'>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id='submit' onClick={getMessages}>➢</div>
+          </div>
+          <p className='info'>Press ➢ to send message</p>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
